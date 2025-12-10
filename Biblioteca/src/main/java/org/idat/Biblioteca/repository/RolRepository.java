dd7df5c0d7a3e79aa4b1f15f9d6c71a5554a2df3
Juan27003 <SM74883636@idat.pe>
2025-12-10 13:18:14 -0500

    Delete Biblioteca directory

--- Biblioteca/src/main/java/org/idat/Biblioteca/repository/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package org.idat.Biblioteca.repository;
-
-import org.idat.Biblioteca.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
